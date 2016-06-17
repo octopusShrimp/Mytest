a0079aac2047f88415692589ee28f653e1d0c33f
chaoyang <chaoyang@janezt.com>
2016-06-17 11:17:19 +0800

    测试 合并2

---
+++ test/test/ViewController2.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  test
+//
+//  Created by admin on 16/6/16.
+//  Copyright © 2016年 chaoyang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"sda");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
