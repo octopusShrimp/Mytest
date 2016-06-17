f3424aabdca70a632e9df549bf9449620077cef1
chaoyang <chaoyang@janezt.com>
2016-06-17 11:43:36 +0800

    no message

--- test/test/ViewController.m
+++ test/test/ViewController.m
@@ -18,11 +18,6 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"sda");
-     NSLog(@"sda");
-     NSLog(@"sda");
-     NSLog(@"sda");
-     NSLog(@"sda");
-     NSLog(@"sda");
 }
 
 - (void)didReceiveMemoryWarning {
